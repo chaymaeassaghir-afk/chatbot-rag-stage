c483cef77b7a86895e7c919a8b95cd530c445ff6
chaymaeassaghir-afk <chaymaeassaghir@gmail.com>
2026-07-09 16:08:21 +0100

    Add files via upload

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,1 +1,50 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
+from app.services.jwt_service import JWTService
+
+security = HTTPBearer()
+
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+
+    token = credentials.credentials
+
+    try:
+
+        payload = JWTService.decode_access_token(token)
+
+        return {
+            "username": payload["sub"],
+            "role": payload["role"]
+        }
+
+    except Exception:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Token invalide"
+        )
+
+
+def require_admin(
+    current_user=Depends(get_current_user)
+):
+
+    if current_user["role"] != "ADMIN":
+
+        raise HTTPException(
+            status_code=403,
+            detail="Accès réservé aux administrateurs."
+        )
+
+    return current_user
+
+
+def require_user(
+    current_user=Depends(get_current_user)
+):
+
+    return current_user
