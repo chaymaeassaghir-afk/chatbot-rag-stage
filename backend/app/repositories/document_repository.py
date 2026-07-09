36a2a3cd17ad6bb77e1dbe5f4e1b4e3c611043d6
chaymaeassaghir-afk <chaymaeassaghir@gmail.com>
2026-07-09 16:07:16 +0100

    Add files via upload

--- backend/app/repositories/document_repository.py
+++ backend/app/repositories/document_repository.py
@@ -1,1 +1,30 @@
+from sqlalchemy.orm import Session
+from app.models.document import Document
 
+
+class DocumentRepository:
+
+    @staticmethod
+    def create(db: Session, filename: str):
+
+        document = Document(filename=filename)
+
+        db.add(document)
+
+        db.commit()
+
+        db.refresh(document)
+
+        return document
+
+
+    @staticmethod
+    def get_all(db: Session):
+
+        return db.query(Document).all()
+
+
+    @staticmethod
+    def get_by_id(db: Session, document_id: int):
+
+        return db.query(Document).filter(Document.id == document_id).first()
